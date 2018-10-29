3efed52eee60482bd9a78a8c34b51d2539e106fb
haiamo <heyuntao@hotmail.com>
2018-10-29 10:58:26 +0800

    PCLTyre's local repository is initialized.

---
+++ stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+#define _SCL_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <tchar.h>
+#include <pcl/io/io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <iostream>
+
+
+
+// TODO:  在此处引用程序需要的其他头文件
